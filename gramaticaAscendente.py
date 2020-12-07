d51006fd10625eea86d898521f1802d8f2b75f60
Jossie Bismarck Castrillo Fajardo <bkcastrillo@gmail.com>
2020-12-07 12:02:34 -0600

    Add files via upload

---
+++ gramaticaAscendente.py
@@ -0,0 +1,281 @@
+
+# -----------------------------------------------------------------------------
+# Grupo 6
+#
+# Universidad de San Carlos de Guatemala
+# Facultad de Ingenieria
+# Escuela de Ciencias y Sistemas
+# Organizacion de Lenguajes y Compiladores 2
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+#                       INICIA ANALIZADOR LEXICO
+# -----------------------------------------------------------------------------
+#palabras reservadas del lenguaje
+reservadas = {
+    #   PALABRAS RESERVADAS POR SQL
+    'show' : 'SHOW',
+    'databases' : 'DATABASES',
+    'tables' : 'TABLES',
+    'columns' : 'COLUMNS',
+    'from' : 'FROM',
+    'select' : 'SELECT',
+    'distinct' : 'DISTINCT',
+    'limit' : 'LIMIT',
+    'offset' : 'OFFSET',    
+    'order' : 'ORDER',
+    'by' : 'BY',
+    'where' : 'WHERE',
+    'and' : 'AND',
+    'or' : 'OR',
+    'not' : 'NOT',
+    'in' : 'IN',
+    'concat' : 'CONCAT',
+    'as' : 'AS',
+    'upper' : 'UPPER',
+    'sqrt' : 'SQRT',
+    'avg' : 'AVG',
+    'sum' : 'SUM',
+    'desc' : 'DESC',
+    'asc' : 'ASC',
+    'like' : 'LIKE',
+    'min' : 'MIN',
+    'max' : 'MAX',
+    'on' : 'ON',
+    'union' : 'UNION',
+    'all' : 'ALL',
+    'insert' : 'INSERT',
+    'into' : 'INTO',
+    'values' : 'VALUES',
+    'update' : 'UPDATE',
+    'set' : 'SET',
+    'delete' : 'DELETE',
+    'create' : 'CREATE',
+    'primary' : 'PRIMARY',
+    'key' : 'KEY',
+    'null' : 'NULL',
+    'unique' : 'UNIQUE',
+    'check' : 'CHECK',
+    'default' : 'DEFAULT',
+    'auto_increment' : 'AUTO_INCREMENT',
+    'alter' : 'ALTER',
+    'table' : 'TABLE',
+    'add' : 'ADD',
+    'drop' : 'DROP',
+    'column' : 'COLUMN',
+    'rename' : 'RENAME',
+    'to' : 'TO',
+    'view' : 'VIEW',
+    'replace' : 'REPLACE',
+    'type' : 'TYPE',
+    'enum' : 'ENUM',
+    'if' : 'IF',
+    'exists' : 'EXISTS',
+    'mode' : 'MODE',
+    'owner' : 'OWNER',
+    'constraint' : 'CONSTRAINT',
+    'foreign' : 'FOREIGN',
+    'references' : 'REFERENCES',
+    'inherits' : 'INHERITS',
+    'group' : 'GROUP',
+    'having' : 'HAVING',
+    'substring' : 'SUBSTRING',
+    'inner' : 'INNER',
+    'outer' : 'OUTER',
+    'izquierda' : 'LEFT',
+    'derecha' : 'RIGHT',
+    'full' : 'FULL',
+    'join' : 'JOIN',
+    'natural' : 'NATURAL',
+    'case' : 'CASE',
+    'when' : 'WHEN',
+    'then' : 'THEN',
+    'begin' : 'BEGIN',
+    'end' : 'END',
+    'else' : 'ELSE',
+    'greatest' : 'GREATEST',
+    'least' : 'LEAST',
+    'intersect' : 'INTERSECT',
+    'except' : 'EXCEPT',
+    #  tipos de datos permitidos
+    'smallint' : 'SMALLINT',
+    'integer' : 'INTEGER',
+    'bigint' : 'BIGINT',
+    'decimal' : 'DECIMAL',
+    'numeric' : 'NUMERIC',
+    'real' : 'REAL',
+    'double' : 'DOUBLE',
+    'money' : 'MONEY',
+    'varying' : 'VARYING',
+    'varchar' : 'VARCHAR',
+    'character' : 'CHARACTER',
+    'char' : 'CHAR',
+    'text' : 'TEXT',
+    'boolean' : 'BOOLEAN',
+    'declare' : 'DECLARE',
+    'function' : 'FUNCTION',
+    'returns' : 'RETURNS',
+    'between' : 'BETWEEN',
+    'ilike' : 'BOOLEAN',
+    'similar' : 'BOOLEAN',
+
+
+
+# revisar funciones de tiempo y fechas
+}
+# listado de tokens que manejara el lenguaje (solo la forma en la que los llamare  en las producciones)
+tokens  = [
+    'PUNTOYCOMA',
+    'MAS',
+    'MENOS',
+    'POR',
+    'DIV',
+
+    'DECIMAL',
+    'ENTERO',
+    'CADENA',
+    'ETIQUETA',
+    'ID'
+] + list(reservadas.values())
+
+# Tokens y la forma en la que se usaran en el lenguaje
+t_PUNTOYCOMA        = r';'
+t_MAS               = r'\+'
+t_MENOS             = r'-'
+t_POR               = r'\*'
+t_DIV               = r'/'
+
+
+
+#definife la estructura de los decimales
+def t_DECIMAL(t):
+    r'\d+\.\d+'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("El valor decimal es muy largo %d", t.value)
+        t.value = 0
+    return t
+#definife la estructura de los enteros
+def t_ENTERO(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("El valor del entero es muy grande %d", t.value)
+        t.value = 0
+    return t
+#definife la estructura de los identificadores que en este caso seran $letra y numero
+def t_ID(t):
+     r'\$[t|a|v|ra|s|p]+[0-9]*'
+     t.type = reservadas.get(t.value.lower(),'ID')    # Check for reserved words
+     return t
+#definife la estructura de las cadenas
+def t_CADENA(t):
+    r'\'.*?\''
+    t.value = t.value[1:-1] # quito las comillas del inicio y final de la cadena
+    return t 
+#definife la estructura de las etiquetas, por el momento las tomo unicamente como letras y numeros
+def t_ETIQUETA(t):
+     r'[a-zA-Z0-9]+'
+     t.type = reservadas.get(t.value.lower(),'ETIQUETA')    # Check for reserved words
+     return t
+
+# Comentario simple # ...
+def t_COMENTARIO_SIMPLE(t):
+    r'--.*\n'
+    t.lexer.lineno += 1
+
+
+# ----------------------- Caracteres ignorados -----------------------
+# caracter equivalente a un tab
+t_ignore = " \t"
+#caracter equivalente a salto de linea
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print("Caracter lexico no permitido ==> '%s'" % t.value)
+    t.lexer.skip(1)
+
+# Construyendo el analizador léxico
+import ply.lex as lex
+lexer = lex.lex()
+
+
+# -----------------------------------------------------------------------------
+#                       INICIA ANALIZADOR SINTACTICO
+# -----------------------------------------------------------------------------
+
+# Asociación de operadores y precedencia
+precedence = (
+    ('left','MAS','MENOS'),
+    ('left','POR','DIV')
+    )
+
+
+
+# estructura de mi gramatica
+
+
+
+def p_inicio(t) :
+    'inicio               : operacion' 
+    t[0]=t[1]  
+    
+
+# empiezan las producciones de las operaciones finales
+#la englobacion de las operaciones
+def p_operacion(t):
+    '''operacion        : ENTERO MAS ENTERO
+                        | ENTERO MENOS ENTERO
+                        '''
+    if (t[2]=="+"):
+        t[0]=t[1]+t[3]
+    elif (t[2]=="-"):
+        t[0]=t[1]-t[3]
+
+
+
+
+
+
+
+
+#para manejar los errores sintacticos
+#def p_error(t): #en modo panico :v
+  #  print("token error: ",t)
+   # print("Error sintáctico en '%s'" % t.value[0])
+   # print("Error sintáctico en '%s'" % t.value[1])
+    
+
+#def p_error(t): #en modo panico :v
+#   while True:
+#        tok=parser.token()
+#        if not tok or tok.type==';':break
+#    parser.errok()
+#    return tok
+def find_column(input, token):
+    line_start = input.rfind('\n', 0, token.lexpos) + 1
+    #print((token.lexpos - line_start) +1 )
+    return (token.lexpos - line_start) 
+
+
+def p_error(t):
+     print("token: '%s'" %t)
+     print("Error sintáctico en '%s' " % t.value)
+     #h.filapivote+=1
+     # Read ahead looking for a closing '}'
+     while True:
+         tok = parser.token()             # Get the next token
+         if not tok or tok.type == 'PUNTOYCOMA': 
+             break
+     parser.restart()
+     
+import ply.yacc as yacc
+parser = yacc.yacc()
+
+def parse(input) :
+    return parser.parse(input)
---
+++ ventana.py
@@ -0,0 +1,324 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+#from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+import accionesIDE as accionesVarias
+import mostrarLineas
+#from random import seed
+#from random import randint
+import ascendente as analizador
+
+
+class SyntaxHighlighter(QtGui.QSyntaxHighlighter):
+    def __init__(self, parent):
+        super(SyntaxHighlighter, self).__init__(parent)
+        self._highlight_lines = dict()
+
+    def highlight_line(self, line, fmt):
+        if isinstance(line, int) and line >= 0 and isinstance(fmt, QtGui.QTextCharFormat):
+            self._highlight_lines[line] = fmt
+            tb = self.document().findBlockByLineNumber(line)
+            self.rehighlightBlock(tb)
+
+    def clear_highlight(self):
+        self._highlight_lines = dict()
+        self.rehighlight()
+
+    def highlightBlock(self, text):
+        line = self.currentBlock().blockNumber()
+        fmt = self._highlight_lines.get(line)
+        if fmt is not None:
+            self.setFormat(0, len(text), fmt)
+
+
+#clase central de mi ventana a mostrarse
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        c=accionesVarias.accionesIde()
+        MainWindow.setObjectName("MainWindow")
+        #tamaño que tendra la ventana 1400px x 900px
+        MainWindow.resize(1400, 900)
+        #le indico que se abra desde el centro de la pantalla pooniendolo en el objeto ventanaCentrada
+        self.ventanaCentrada = QtWidgets.QWidget(MainWindow)
+        self.ventanaCentrada.setObjectName("ventanaCentrada")
+        #se crea etiqueta
+        #self.EdicionTexto = QtWidgets.QPlainTextEdit(self.ventanaCentrada)
+        self.EdicionTexto = mostrarLineas.QCodeEditor(self.ventanaCentrada)
+        
+        #para definir su posicion
+        self.EdicionTexto.setGeometry(QtCore.QRect(40, 70, 1290, 500))
+        font = QtGui.QFont()
+        # para cambiar el tamaño de la fuente de un objeto
+        font.setPointSize(16)
+        self.EdicionTexto.setFont(font)
+        self.EdicionTexto.setObjectName("EdicionTexto")
+
+       
+
+        #area de consola de salida
+        self.ConsolaSalida = QtWidgets.QTextEdit(self.ventanaCentrada)
+        self.ConsolaSalida.setGeometry(QtCore.QRect(40, 590, 1290, 260))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.ConsolaSalida.setFont(font)
+        self.ConsolaSalida.setObjectName("ConsolaSalida")
+
+        MainWindow.setCentralWidget(self.ventanaCentrada)
+        #fijo la barra de menu en la parte superior
+        self.barraMenu = QtWidgets.QMenuBar(MainWindow)
+        self.barraMenu.setGeometry(QtCore.QRect(0, 0, 800, 36))
+        self.barraMenu.setObjectName("barraMenu")
+        #archivo
+        self.archivoMenu = QtWidgets.QMenu(self.barraMenu)
+        self.archivoMenu.setObjectName("archivoMenu")
+        #editar
+        self.editarMenu = QtWidgets.QMenu(self.barraMenu)
+        self.editarMenu.setObjectName("editarMenu")
+        #ejecutar
+        self.ejecutarMenu = QtWidgets.QMenu(self.barraMenu)
+        self.ejecutarMenu.setObjectName("ejecutarMenu")
+        #etiquetas de iconos
+        self.iconoAbrir = QtWidgets.QPushButton(self.ventanaCentrada)
+        self.iconoAbrir.setGeometry(QtCore.QRect(40, 10, 50, 50))
+        self.iconoAbrir.setObjectName("iconoAbrir")
+        
+
+
+        self._highlighter = SyntaxHighlighter(self.EdicionTexto.document())
+
+        self.iconoNuevo = QtWidgets.QPushButton(self.ventanaCentrada)
+        self.iconoNuevo.setGeometry(QtCore.QRect(90, 10, 50, 50))
+        self.iconoNuevo.setObjectName("iconoNuevo")
+
+        self.iconoGuardar = QtWidgets.QPushButton(self.ventanaCentrada)
+        self.iconoGuardar.setGeometry(QtCore.QRect(140, 10, 50, 50))
+        self.iconoGuardar.setObjectName("iconoGuardar")
+
+        self.iconoGuardarComo = QtWidgets.QPushButton(self.ventanaCentrada)
+        self.iconoGuardarComo.setGeometry(QtCore.QRect(190, 10, 50, 50))
+        self.iconoGuardarComo.setObjectName("iconoGuardarComo")
+
+        self.iconoDescendente = QtWidgets.QPushButton(self.ventanaCentrada)
+        self.iconoDescendente.setGeometry(QtCore.QRect(300, 10, 50, 50))
+        self.iconoDescendente.setObjectName("iconoDescendente")
+
+        self.iconoAscendente = QtWidgets.QPushButton(self.ventanaCentrada)
+        self.iconoAscendente.setGeometry(QtCore.QRect(350, 10, 50, 50))
+        self.iconoAscendente.setObjectName("iconoAscendente")
+
+
+
+        #------------------------------------------------
+        MainWindow.setMenuBar(self.barraMenu)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        #acciones de Archivo
+        self.accionNuevo = QtWidgets.QAction(MainWindow)
+        self.accionNuevo.setObjectName("accionNuevo")
+        self.accionAbrir = QtWidgets.QAction(MainWindow)
+        self.accionAbrir.setObjectName("accionAbrir")
+        self.accionGuardar = QtWidgets.QAction(MainWindow)
+        self.accionGuardar.setObjectName("accionGuardar")
+        self.accionGuardarComo = QtWidgets.QAction(MainWindow)
+        self.accionGuardarComo.setObjectName("accionGuardarComo")
+        self.accionCerrar = QtWidgets.QAction(MainWindow)
+        self.accionCerrar.setObjectName("accionCerrar")
+        self.accionSalir = QtWidgets.QAction(MainWindow)
+        self.accionSalir.setObjectName("accionSalir")
+        
+        self.archivoMenu.addAction(self.accionNuevo)
+        self.archivoMenu.addAction(self.accionAbrir)
+        self.archivoMenu.addAction(self.accionGuardar)
+        self.archivoMenu.addAction(self.accionGuardarComo)
+        self.archivoMenu.addAction(self.accionCerrar)
+        self.archivoMenu.addAction(self.accionSalir)
+        #acciones de editar
+
+
+
+        #acciones Ejecutar
+        self.accionDescendente = QtWidgets.QAction(MainWindow)
+        self.accionDescendente.setObjectName("accionDescendente")
+        self.accionAscendente = QtWidgets.QAction(MainWindow)
+        self.accionAscendente.setObjectName("accionAscendente")
+        self.accionPasos = QtWidgets.QAction(MainWindow)
+        self.accionPasos.setObjectName("accionPasos")
+        self.accionSimbolos = QtWidgets.QAction(MainWindow)
+        self.accionSimbolos.setObjectName("accionsimbolos")
+        self.accionAST = QtWidgets.QAction(MainWindow)
+        self.accionAST.setObjectName("accionAST")
+        self.accionGramatical = QtWidgets.QAction(MainWindow)
+        self.accionGramatical.setObjectName("accionGramatical")
+        self.accionErrores = QtWidgets.QAction(MainWindow)
+        self.accionErrores.setObjectName("accionErrores")
+
+        self.ejecutarMenu.addAction(self.accionDescendente)
+        self.ejecutarMenu.addAction(self.accionAscendente)
+        self.ejecutarMenu.addAction(self.accionPasos)
+        self.ejecutarMenu.addAction(self.accionSimbolos)
+        self.ejecutarMenu.addAction(self.accionAST)
+        self.ejecutarMenu.addAction(self.accionGramatical)
+        self.ejecutarMenu.addAction(self.accionErrores)
+        #acciones de opciones
+
+
+
+
+        #genera los botones y su accion de despliegue
+        self.barraMenu.addAction(self.archivoMenu.menuAction())
+        self.barraMenu.addAction(self.ejecutarMenu.menuAction())
+        #acciones de los botones de cada menu
+        
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        #funciones de archivo
+        self.accionNuevo.triggered.connect(lambda: self.EdicionTexto.insertPlainText(""))
+        self.accionAbrir.triggered.connect(self.abrir_archivo)
+        self.accionGuardar.triggered.connect(lambda: self.guardar_normal(self.EdicionTexto.toPlainText()))
+        self.accionGuardarComo.triggered.connect(lambda: self.guardar_como(self.EdicionTexto.toPlainText()))
+        self.accionCerrar.triggered.connect(lambda: c.cerrarVentana(QtWidgets.QApplication(sys.argv)))
+        self.accionSalir.triggered.connect(lambda: c.cerrarVentana(QtWidgets.QApplication(sys.argv)))
+        #funciones de ejecutar
+        self.accionAscendente.triggered.connect(self.ejecutar_ascendente)
+
+
+
+
+      
+        #acciones de los botoncitos destemplados :v
+        self.iconoAbrir.clicked.connect(self.abrir_archivo)
+        self.iconoGuardar.clicked.connect(self.guardar_normal)
+        self.iconoGuardarComo.clicked.connect(self.guardar_como)
+
+        self.iconoAscendente.clicked.connect(self.ejecutar_ascendente)
+        self.iconoNuevo.clicked.connect(lambda: self.EdicionTexto.insertPlainText(""))
+
+    def retranslateUi(self, MainWindow):
+        #aca solo setteo los nombres en cada componente y un sub nombre por asi decirlo
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Augus - Jossie Castrillo 201313692"))
+        self.EdicionTexto.insertPlainText(_translate("MainWindow", ""))
+        self.archivoMenu.setTitle(_translate("MainWindow", "Archivo"))
+        self.ejecutarMenu.setTitle(_translate("MainWindow", "Ejecutar"))
+        # de archivo
+        self.accionNuevo.setText(_translate("MainWindow", "Nuevo"))
+        self.accionNuevo.setShortcut(_translate("MainWindow", "Ctrl+N"))
+        self.accionAbrir.setText(_translate("MainWindow", "Abrir"))
+        self.accionAbrir.setShortcut(_translate("MainWindow", "Ctrl+O"))
+        self.accionGuardar.setText(_translate("MainWindow", "Guardar"))
+        self.accionGuardar.setShortcut(_translate("MainWindow", "Ctrl+S"))
+        self.accionGuardarComo.setText(_translate("MainWindow", "Guardar Como"))
+        self.accionGuardarComo.setShortcut(_translate("MainWindow", "Ctrl+P"))
+        self.accionCerrar.setText(_translate("MainWindow", "Cerrar"))
+        self.accionCerrar.setShortcut(_translate("MainWindow", "Ctrl+W"))
+        self.accionSalir.setText(_translate("MainWindow", "Salir"))
+        self.accionSalir.setShortcut(_translate("MainWindow", "Ctrl+E"))
+        #de ejecutar
+        self.accionDescendente.setText(_translate("MainWindows","Analisis Descendente"))
+        self.accionAscendente.setText(_translate("MainWindows","Analisis Ascendente"))
+        self.accionPasos.setText(_translate("MainWindows","Analisis paso a paso"))
+        self.accionSimbolos.setText(_translate("MainWindows","Tabla de simbolos"))
+        self.accionAST.setText(_translate("MainWindows","AST"))
+        self.accionGramatical.setText(_translate("MainWindows","Reporte Gramatical"))
+        self.accionErrores.setText(_translate("MainWindows","Reporte de Errores"))
+ 
+
+        self.iconoAbrir.setText(_translate("MainWindows","Open"))
+        self.iconoNuevo.setText(_translate("MainWindows","nuevo"))
+        self.iconoGuardar.setText(_translate("MainWindows","save"))
+        self.iconoGuardarComo.setText(_translate("MainWindows","save as"))
+        self.iconoDescendente.setText(_translate("MainWindows","desc"))
+        self.iconoAscendente.setText(_translate("MainWindows","asc"))
+
+
+#self.EdicionTexto.setText(c.abrirArchivo("C:\\Users\\Jossie Castrillo\\Desktop\\[OLC2]P1_201313692\\prueba.txt"))
+    def clicked(self, text):
+        self.EdicionTexto.insertPlainText(path)
+        print(path)
+        #self.EdicionTexto.adjustSize()
+    #metodo para cambiar el color del fondo del entorno de escritura unicamente (falta mejorar)
+    
+    #metodo para mostrar la ventana emergente con los datos del desarrollador, osea yo
+    def ventanaAcercaDe(self,t):
+        print(t)
+        msg=QtWidgets.QMessageBox()
+        msg.setWindowTitle("Acerca De...")
+        msg.setText("Jossie Bismarck Castrillo Fajardo - 201313692")
+        msg.exec_()
+    #metodo para ventana emergente generica
+    def ventanaEmergente(self,mensaje):
+        msg=QtWidgets.QMessageBox()
+        msg.setWindowTitle("Notificacion")
+        msg.setText(mensaje)
+        msg.exec_()
+    #este metodo genera el file chooser donde se podra buscar el archivo y devuelve la ruta del archivo
+    #posteriormente se manda la ruta el meetodo c.abrirArchivo del import accionesIDE y devuelve el texto abierto
+    def abrir_archivo(self):
+        try:
+            c=accionesVarias.accionesIde()
+            dialog = QtWidgets.QFileDialog()
+            fname = dialog.getOpenFileName(None, "Abrir archivo", "", "all (*.*)")
+            global path
+            path=fname[0]
+            self.EdicionTexto.insertPlainText(c.abrirArchivo(path))
+            print(path)
+        except:
+            self.ventanaEmergente("no se selecciono ningun archivo")
+    #envia el texto del editor y la ruta de un archivo ya abierto al metodo del import accionesIDE de guardarArchivo
+    def guardar_como(self,texto):
+        try:
+            c=accionesVarias.accionesIde()
+            name = QtWidgets.QFileDialog.getSaveFileName(None, 'Save File')
+            print(name[0])
+            if c.guardarArchivo(name[0],texto):
+                self.ventanaEmergente("Se ha guardado su archivo exitosamente!")
+            #abre un file chooser para seleccionar un lugar para guardar el archivo y permite escribir el nombre con su extension.
+            #esa ruta se envia con el texto del editor al metodo guardarArchivo
+        except:
+            self.ventanaEmergente("no se guardo el archivo")
+    def guardar_normal(self,texto):
+        try:
+            c=accionesVarias.accionesIde()
+            if c.guardarArchivo(path,texto):
+                self.ventanaEmergente("Se ha guardado su archivo exitosamente!")
+        except:
+            self.ventanaEmergente("la ruta no se ha definido")
+
+
+    def ejecutar_ascendente(self):
+        x=self.EdicionTexto.toPlainText()
+        y=self.ConsolaSalida.toPlainText()
+        try:
+            salida=analizador.ejecucionAscendente(x,y)
+            salida+="\n---------------------FIN EJECUCION ASCENDENTE--------------------------\n"
+        except:
+            salida="Grupo6>Se genero un error de analisis"
+        self.ConsolaSalida.setPlainText(salida)
+
+    
+        #print(x)
+    
+
+
+
+    
+    def takeinputs(self): 
+        name, done1 = QtWidgets.QInputDialog.getText( 
+            self.ventanaCentrada, 'Input Dialog', 'Enter your name:') 
+        if done1: 
+             # Showing confirmation message along 
+             # with information provided by user.  
+            print(name)
+            return name
+        return "j"
+        
+
+#inicializacion del main y su interfaz para ejecucion
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
